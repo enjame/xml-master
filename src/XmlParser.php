d33ef693ea2daaaef12c5c005212fa11e011bae9
Enjame <enjame74@gmail.com>
2018-07-30 20:35:39 +0500

    start init

---
+++ src/XmlParser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Enjame\Xml;
+
+class XmlParser
+{
+    protected $content;
+
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+
+    public function xml2array()
+    {
+        $xml = simplexml_load_string($this->content);
+        $json = json_encode($xml);
+
+        return json_decode($json, TRUE);
+    }
+
+
+    public function array2xml()
+    {
+
+    }
+}
