d6d4681aa5da1cf88f30df79b1b0f397d70f0bc9
Enjame <enjame74@gmail.com>
2018-07-31 16:24:22 +0500

    added xml2array

--- src/XmlParser.php
+++ src/XmlParser.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace Enjame\Xml;
+
+use phpQuery;
 
 class XmlParser
 {
@@ -13,15 +16,60 @@ class XmlParser
 
     public function xml2array()
     {
-        $xml = simplexml_load_string($this->content);
-        $json = json_encode($xml);
+        $result = [];
 
-        return json_decode($json, TRUE);
+        $xmlDocument = phpQuery::newDocument($this->content);
+
+        return $this->getElements($xmlDocument);
     }
 
 
     public function array2xml()
     {
 
+    }
+
+    protected function getElements($root)
+    {
+        $phpQueryObject = pq($root);
+        $arr = [];
+        $result = $phpQueryObject->children();
+        $hasChilds = [];
+        foreach ($result as $child) {
+            if (array_key_exists($child->tagName, $arr)) {
+                if (!in_array($child->tagName, $hasChilds)) {
+                    $hasChilds[] = $child->tagName;
+                    $tmpChild = $arr[$child->tagName];
+                    $arr[$child->tagName] = [];
+                    $arr[$child->tagName][] = $tmpChild;
+                }
+
+                $arr[$child->tagName][] = $this->getItem($child);
+
+            } else {
+                $arr[$child->tagName] = $this->getItem($child);
+            }
+        }
+        return $arr;
+    }
+
+    protected function getItem($root)
+    {
+        $childText = preg_replace('/[\\\\\r\n]\s\s+/', '', pq($root)->contents()->eq(0)->text());
+        $childAttrs = pq($root)->attr('*');
+        $childs = $this->getElements($root);
+        $arr = [
+            '#value' => $childText,
+        ];
+
+        foreach ($childAttrs as $key => $value) {
+            $arr['@' . $key] = $value;
+        }
+
+        foreach ($childs as $key => $item) {
+            $arr[$key] = $item;
+        }
+
+        return $arr;
     }
 }
